160b2d0fc256848fad37f75f42fba2411ba2f7ed
baikaishuipp <441640312@qq.com>
2024-05-06 13:44:42 +0800

    Fix Bug #11

--- src/jcci/constant.py
+++ src/jcci/constant.py
@@ -22,4 +22,14 @@ DIFF_TYPE_CHANGED = 'changed'
 DIFF_TYPE_IMPACTED = 'impacted'
 
 JAVA_BASIC_TYPE = ['string', 'int', 'boolean', 'long', 'byte', 'short', 'float', 'double', 'char']
+JAVA_UTIL_TYPE = [
+    'ArrayList', 'Base64', 'Calendar', 'Collection', 'Collections', 'Comparators', 'Date', 'Dictionary',
+    'EnumMap', 'EnumSet', 'EventListener', 'EventObject', 'Formatter',
+    'HashMap', 'HashSet', 'Hashtable', 'Iterator', 'LinkedHashMap', 'LinkedHashSet', 'LinkedList',
+    'List', 'ListIterator', 'Locale', 'Map', 'NavigableMap', 'NavigableSet', 'Objects',
+    'Optional', 'OptionalDouble', 'OptionalInt', 'OptionalLong', 'Properties', 'Queue', 'Random',
+    'RegularEnumSet', 'ResourceBundle', 'Scanner', 'ServiceLoader', 'Set', 'SimpleTimeZone',
+    'SortedMap', 'SortedSet', 'Spliterator', 'Spliterators', 'SplittableRandom', 'Stack', 'StringJoiner',
+    'StringTokenizer', 'TaskQueue', 'Timer', 'TimerTask', 'TimerThread', 'TimeZone', 'TimSort', 'TreeMap',
+    'TreeSet', 'Tripwire', 'UUID', 'Vector', 'WeakHashMap']
 MAPPING_LIST = ['PostMapping', 'GetMapping', 'DeleteMapping', 'PutMapping', 'PatchMapping', 'RequestMapping']
